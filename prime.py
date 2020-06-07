fcac4eef75faf6590b857681c9531e9dd04c0435
Karl Zeilinger <kzeilinger@gmail.com>
2020-06-07 04:42:30 -0400

    travis fix to pass

--- prime.py
+++ prime.py
@@ -9,7 +9,7 @@ def generate_prime_factors(input_num):
     div = 2
     try:
         while input_num > 1:
-            if(input_num % div == 0):
+            if input_num % div == 0:
                 prime_list.append(div)
                 input_num = input_num / div
                 div = div - 1
